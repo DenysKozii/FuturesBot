72d00bde7569bc9705da95026dbbe420262029a0
denys <denys.kozii@gmail.com>
2022-07-26 19:09:56 +0300

    commit

--- src/main/java/crypto/bot/modes/Live.java
+++ src/main/java/crypto/bot/modes/Live.java
@@ -44,8 +44,8 @@ public final class Live {
         try {
             List<Currency> currencies = new ArrayList<>();
             for (String symbol : ConfigSetup.getCurrencies()) {
-                for (int deltaRSI = 5; deltaRSI <= 5; deltaRSI += 5) {
-                    for (double deltaStop = 0.01; deltaStop <= 0.01; deltaStop += 0.005) {
+                for (int deltaRSI = -20; deltaRSI <= 5; deltaRSI += 5) {
+                    for (double deltaStop = 0.007; deltaStop <= 0.019; deltaStop += 0.005) {
                         Optional<Trade> tradeOptionalROE = tradeRepository.findBySymbolAndLongRSIAndShortRSIAndStopAndStrategy(symbol, 30 + deltaRSI, 70 - deltaRSI, deltaStop, Strategy.ROE);
                         upsert(currencies, symbol, deltaRSI, deltaStop, tradeOptionalROE, Strategy.ROE);
                     }
